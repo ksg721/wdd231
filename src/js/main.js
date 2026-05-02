100b0b8d690a0f0ce4634760b7114cf1cb05dc7e
ksg721 <gre20018@byui.edu>
2026-05-01 18:12:05 -0600

    test to see if us map shows

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-function loadParkData() {
-  document.getElementById("parkName").textContent = "Yellowstone";
-  document.getElementById("parkType").textContent = "National Park";
-  document.getElementById("parkStates").textContent = "WY, ID, MT";
-  document.querySelector("#heroImage").src = "./images/yellowstone.jpg";
-}
+// function loadParkData() {
+//   document.getElementById("parkName").textContent = "Yellowstone";
+//   document.getElementById("parkType").textContent = "National Park";
+//   document.getElementById("parkStates").textContent = "WY, ID, MT";
+//   document.querySelector("#heroImage").src = "/public/images/yellowstone.jpg";
+// }
 
-loadParkData();
+// loadParkData();
