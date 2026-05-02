e629ef92be8c7a4eabcd7a06bceb49bc9e314d08
ksg721 <gre20018@byui.edu>
2026-05-01 18:58:26 -0600

    completed prove 02

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,32 @@ function updateParkInformation(){
     document.getElementById("heroImage").src = "images/yellowstone.jpg"
 }
 
+function addEventListeners() {
+    const menuTrigger = document.querySelector("#header-menu-trigger")
+    const menuOptions = document.querySelector("#header-menu-options")
+    const parkInfo = document.querySelector("#parkInfo")
+
+    if (menuTrigger && menuOptions) {
+        menuTrigger.addEventListener("click", () => {
+            menuOptions.classList.toggle("is-hidden")
+        })
+
+        menuOptions.addEventListener("click", (event) => {
+            const itemName = event.target.textContent
+            console.log(itemName)
+        })
+    }
+
+    if (parkInfo) {
+        parkInfo.addEventListener("mouseenter", () => {
+            parkInfo.classList.add("overlay-hover")
+        })
+
+        parkInfo.addEventListener("mouseleave", () => {
+            parkInfo.classList.remove("overlay-hover")
+        })
+    }
+}
+
 updateParkInformation()
+addEventListeners()
